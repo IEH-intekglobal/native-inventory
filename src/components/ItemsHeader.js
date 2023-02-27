2cf2602bf2d8c6d955a5f4ac9a960658621be240
javier171188 <javier171188@hotmail.com>
2023-02-27 13:59:55 -0800

    Basic summary added

--- src/components/ItemsHeader.js
+++ src/components/ItemsHeader.js
@@ -1,5 +1,50 @@
-import { Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { items } from "../db/dummy-items";
+
+function ItemInfo({ name, info }) {
+  return (
+    <View>
+      <Text>{name}</Text>
+      <Text style={styles.info}>{info}</Text>
+    </View>
+  );
+}
 
 export function ItemsHeader() {
-  return <Text>Sumary</Text>;
+  const numberItems = items.length;
+  const totalUnities = items.reduce((accumulate, currentItem) => {
+    return currentItem.quantity + accumulate;
+  }, 0);
+
+  const totalValue = items.reduce((accumulate, currentItem) => {
+    const currentValue = currentItem.quantity * currentItem.price;
+    return accumulate + currentValue;
+  }, 0);
+  const totalValueStr = `$${String(totalValue / 1000)}K`;
+
+  return (
+    <View style={styles.container}>
+      <ItemInfo name="Folders" info="1" />
+      <ItemInfo name="Items" info={numberItems} />
+      <ItemInfo name="Total units" info={totalUnities} />
+      <ItemInfo name="Total value" info={totalValueStr} />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    backgroundColor: "white",
+    justifyContent: "space-around",
+    borderBottomColor: "gray",
+    borderBottomWidth: 1,
+    borderTopColor: "gray",
+    borderTopWidth: 1,
+    height: 70,
+    alignItems: "center",
+  },
+  info: {
+    fontWeight: "bold",
+  },
+});
